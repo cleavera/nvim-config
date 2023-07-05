ce98417634ec2fede280bf40dc9de6cdb6a9af68
Anthony Cleaver <Anthony.cleaver@mhr.co.uk>
2023-07-05 14:00:51 +0100

    Add gitsigns

--- lua/keymaps/git.lua
+++ lua/keymaps/git.lua
@@ -3,5 +3,42 @@ which_key.register({
   ["<leader>g"] = { name = "Git" }
 })
 
-vim.keymap.set('n', '<leader>gs', "<cmd>Git<cr>", { desc = "Git summary" })
-vim.keymap.set('n', '<leader>gp', "<cmd>Git pull origin --rebase<cr>", { desc = "Git pull" })
+local Git = {}
+
+function Git:get_buf()
+  if self.buf == nil then
+    local buf = vim.api.nvim_create_buf(false, true)
+    vim.api.nvim_buf_set_option(buf, 'filetype', 'GitDiff')
+
+    self.buf = buf
+  end
+
+  return self.buf
+end
+
+function Git:create_win(buf)
+  local win = vim.api.nvim_open_win(buf, true, {
+    relative = 'editor',
+    style = 'minimal',
+    width = 80,
+    height = 20,
+    col = 20,
+    row = 20,
+  })
+
+  return win
+end
+
+function Git:summary()
+  local b = self:get_buf()
+  self:create_win(b)
+end
+
+vim.keymap.set('n', '<leader>gs', "<cmd>Git<cr>", { desc = "Summary" })
+vim.keymap.set('n', '<leader>gp', "<cmd>Git pull origin --rebase<cr>", { desc = "Pull" })
+vim.keymap.set('n', '<leader>gb', function() require("gitsigns").blame_line({ full = true }) end, { desc = "Blame line" })
+vim.keymap.set('n', '<leader>gd', function() require("gitsigns").diffthis() end, { desc = "Diff file" })
+
+--vim.keymap.set('n', '<leader>gt', function()
+--  Git:summary()
+--end, { desc = "Test" })
