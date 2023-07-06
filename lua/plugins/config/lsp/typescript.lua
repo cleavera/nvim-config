2453b37a0a9e0956aac1d635e40e8a2816080dc4
Anthony Cleaver <Anthony.cleaver@mhr.co.uk>
2023-07-06 16:16:33 +0100

    Add lsp actions

--- lua/plugins/config/lsp/typescript.lua
+++ lua/plugins/config/lsp/typescript.lua
@@ -1,9 +1,27 @@
+local function organize_imports()
+  local params = {
+    command = "_typescript.organizeImports",
+    arguments = { vim.api.nvim_buf_get_name(0) },
+    title = ""
+  }
+  vim.lsp.buf.execute_command(params)
+end
+
 return {
   name = "tsserver",
-  setup = function (on_attach, capabilities)
+  setup = function(on_attach, capabilities)
     require("lspconfig").tsserver.setup({
-      on_attach = on_attach,
+      on_attach = function(_, buffer)
+        on_attach(_, buffer)
+        vim.api.nvim_buf_set_keymap(buffer, "n", "<leader>lo", "<cmd>OrganizeImports<cr>", { desc = "Organise imports" })
+      end,
       capabilities = capabilities,
+      commands = {
+        OrganizeImports = {
+          organize_imports,
+          description = "Organize Imports"
+        }
+      }
     })
   end
 }
--- lua/plugins/specs/lsp.lua
+++ lua/plugins/specs/lsp.lua
@@ -9,15 +9,16 @@ return {
     build = ":MasonUpdate",
     config = function()
       local on_attach = function(_, buffer)
-        local function map(...)
+        local function remap_for_buffer(...)
           vim.api.nvim_buf_set_keymap(buffer, ...)
         end
 
         vim.api.nvim_buf_set_option(buffer, "omnifunc", "v:lua.vim.lsp.omnifunc")
-
-        map("n", "<leader>lh", function ()
-          vim.lsp.buf.hover()
-        end)
+        remap_for_buffer("n", "<leader>lh", "<cmd>lua vim.lsp.buf.hover()<cr>", { desc = "Peek" })
+        remap_for_buffer("n", "<leader>lg", "<cmd>lua vim.lsp.buf.definition()<cr>", { desc = "Goto definition" })
+        remap_for_buffer("n", "<leader>lf", "<cmd>lua vim.lsp.buf.format()<cr>", { desc = "Format" })
+        remap_for_buffer("n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", { desc = "Rename" })
+        remap_for_buffer("n", "<leader>ld", "<cmd>lua vim.diagnostic.open_float()<cr>", { desc = "Diagnostics" })
       end
 
       local function get_file_name(file)
@@ -37,7 +38,7 @@ return {
       local lsp_setups = {}
 
       local lsps = require("plenary.scandir").scan_dir(script_path() .. "../config/lsp", { add_dirs = false })
-      for _,lsp in ipairs(lsps) do
+      for _, lsp in ipairs(lsps) do
         local l = require("plugins.config.lsp." .. get_file_name(lsp))
         table.insert(server_names, l.name)
         table.insert(lsp_setups, l.setup)
@@ -49,7 +50,7 @@ return {
         ensure_installed = server_names
       })
 
-      for _,setup in ipairs(lsp_setups) do
+      for _, setup in ipairs(lsp_setups) do
         setup(on_attach, capabilities)
       end
     end
