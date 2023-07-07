4515349d6a2f90b5f78acdf9dc1427122ec89b1e
Anthony Cleaver <Anthony.cleaver@mhr.co.uk>
2023-07-07 17:04:17 +0100

    Add extra git bindings

--- lua/keymaps/buffer.lua
+++ lua/keymaps/buffer.lua
@@ -6,3 +6,33 @@ which_key.register({
 vim.keymap.set('n', '<leader>bc', function()
   vim.api.nvim_buf_delete(vim.api.nvim_get_current_buf(), {})
 end, { desc = "Close" })
+vim.keymap.set('n', '<leader>bj', function()
+  local bufs = vim.api.nvim_list_bufs()
+  local current_buf = vim.api.nvim_get_current_buf()
+  local next_buf = nil
+
+  for i, buf in ipairs(bufs) do
+    if buf == current_buf then
+      next_buf = bufs[i + 1]
+    end
+  end
+
+  if next_buf ~= nil then
+    vim.api.nvim_set_current_buf(next_buf)
+  end
+end, { desc = "Next" })
+vim.keymap.set('n', '<leader>bk', function()
+  local bufs = vim.api.nvim_list_bufs()
+  local current_buf = vim.api.nvim_get_current_buf()
+  local next_buf = nil
+
+  for i, buf in ipairs(bufs) do
+    if buf == current_buf then
+      next_buf = bufs[i - 1]
+    end
+  end
+
+  if next_buf ~= nil then
+    vim.api.nvim_set_current_buf(next_buf)
+  end
+end, { desc = "Previous" })
--- lua/keymaps/git.lua
+++ lua/keymaps/git.lua
@@ -34,10 +34,18 @@ function Git:summary()
   self:create_win(b)
 end
 
-vim.keymap.set('n', '<leader>gs', "<cmd>Git<cr>", { desc = "Summary" })
-vim.keymap.set('n', '<leader>gp', "<cmd>Git pull origin --rebase<cr>", { desc = "Pull" })
+local telescope = require("telescope.builtin")
+
+-- vim.keymap.set('n', '<leader>gs', "<cmd>Git<cr>", { desc = "Summary" })
+vim.keymap.set('n', '<leader>gs', function () telescope.git_status() end, { desc = "Summary" })
+vim.keymap.set('n', '<leader>gu', "<cmd>Git pull origin --rebase<cr>", { desc = "Pull" })
+vim.keymap.set('n', '<leader>gp', "<cmd>Git push origin<cr>", { desc = "Push" })
 vim.keymap.set('n', '<leader>gb', function() require("gitsigns").blame_line({ full = true }) end, { desc = "Blame line" })
 vim.keymap.set('n', '<leader>gd', function() require("gitsigns").diffthis() end, { desc = "Diff file" })
+vim.keymap.set('n', '<leader>gc', function() telescope.git_commits() end, { desc = "Commits" })
+vim.keymap.set('n', '<leader>gC', function() telescope.git_bcommits() end, { desc = "File commits" })
+vim.keymap.set('n', '<leader>gB', function() telescope.git_branches() end, { desc = "Branches" })
+vim.keymap.set('n', '<leader>gS', function() telescope.git_stash() end, { desc = "Stashes" })
 
 --vim.keymap.set('n', '<leader>gt', function()
 --  Git:summary()
