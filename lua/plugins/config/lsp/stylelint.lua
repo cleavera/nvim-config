e0c972959b9c87c5b56b01a7484048d9783df6f9
Anthony Cleaver <Anthony.cleaver@mhr.co.uk>
2023-08-04 10:51:56 +0100

    fix stylelint

--- lua/plugins/config/lsp/stylelint.lua
+++ lua/plugins/config/lsp/stylelint.lua
@@ -1,9 +1,58 @@
 return {
   name = "stylelint_lsp",
-  setup = function (on_attach, capabilities)
-    require("lspconfig").stylelint_lsp.setup({
+  setup = function(on_attach, capabilities)
+    local nvim_lsp = require('lspconfig')
+
+    local filetypes = {
+      css = { 'stylelint' },
+      scss = { 'stylelint' },
+      less = { 'stylelint' },
+    }
+    local linters = {
+      stylelint = {
+        sourceName = 'stylelint',
+        command = 'npx',
+        args = { 'stylelint', '--formatter', 'compact', '%filepath' },
+        rootPatterns = { '.stylelintrc' },
+        debounce = 100,
+        formatPattern = {
+          [[: line (\d+), col (\d+), (warning|error) - (.+?) \((.+)\)]],
+          {
+            line = 1,
+            column = 2,
+            security = 3,
+            message = { 4, ' [', 5, ']' },
+          },
+        },
+        securities = {
+          warning = 'warning',
+          error = 'error',
+        },
+      },
+    }
+
+    local formatters = {
+      stylelint = {
+        command = 'npx',
+        args = { 'stylelint', '--fix', '--stdin', '--stdin-filename', '%filepath' },
+      }
+    }
+    local formatFiletypes = {
+      css = { 'stylelint' },
+      scss = { 'stylelint' },
+      less = { 'stylelint' },
+    }
+
+    nvim_lsp.diagnosticls.setup {
       on_attach = on_attach,
       capabilities = capabilities,
-    })
+      filetypes = vim.tbl_keys(filetypes),
+      init_options = {
+        filetypes = filetypes,
+        linters = linters,
+        formatters = formatters,
+        formatFiletypes = formatFiletypes,
+      }
+    }
   end
 }
