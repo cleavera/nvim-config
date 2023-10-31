995cd5cbeb8351e4fd6e2d20d1039e0d4900cffa
Anthony Cleaver <mr.anthony.cleaver@outlook.com>
2023-10-31 15:18:42 +0000

    Fix block comments

--- lua/keymaps/comment.lua
+++ lua/keymaps/comment.lua
@@ -1,5 +1,5 @@
 local api = require('Comment.api')
 
 vim.keymap.set('n', '<c-_>', function() api.toggle.linewise.current() end, { desc = "Comment" })
-vim.keymap.set('i', '<c-_>', function() api.toggle.linewise.current() end, { desc = "Comment" })
-vim.keymap.set('v', '<c-_>', function() api.toggle.linewise.current() end, { desc = "Comment" })
+vim.keymap.set('i', '<c-_>', function() api.toggle.linewise.current() end, { desc = "comment" })
+vim.keymap.set('v', '<c-_>', function() api.toggle.blockwise(vim.fn.visualmode()) end, { desc = "comment" })
