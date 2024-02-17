97cecaa86cf953770449f19f5adcecf802d51054
Anthony Cleaver <Anthony.cleaver@mhr.co.uk>
2024-02-17 18:33:01 +0000

    Improve project picker display

--- lua/plugins/project/picker.lua
+++ lua/plugins/project/picker.lua
@@ -4,20 +4,47 @@ local sorters = require('telescope.sorters')
 local dropdown = require('telescope.themes').get_dropdown({})
 local actions = require('telescope.actions')
 local action_state = require('telescope.actions.state')
+local entry_display = require('telescope.pickers.entry_display')
 
 local M = {}
+
+local function get_project_name(path)
+  return vim.fn.fnamemodify(path, ":t")
+end
 
 function M:open_picker(source)
   local opts = {
     prompt_title = "Projects",
     sorter = sorters.get_fzy_sorter({}),
-    finder = finders.new_table(source),
+    finder = finders.new_table({
+      results = source,
+      entry_maker = function(entry)
+        local displayer = entry_display.create({
+          separator = " ",
+          items = {
+            {},
+            { remaining = true },
+          },
+        });
+
+        return {
+          value = entry,
+          ordinal = entry,
+          display = function(entry)
+            return displayer {
+              { get_project_name(entry.value), "TelescopeResultsIdentifier" },
+              { entry.value, "NonText" }
+            }
+          end
+        }
+      end
+    }),
     attach_mappings = function(prompt_bufnr, _)
       actions.select_default:replace(function()
         actions.close(prompt_bufnr)
         local selection = action_state.get_selected_entry()
 
-        vim.fn.chdir(selection[1])
+        vim.fn.chdir(selection.value)
         vim.cmd("bufdo bwipeout")
       end)
 
