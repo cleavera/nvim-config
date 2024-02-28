8519e3e1a4e0995bff1dd4d4c88bd67fa8e91b85
Anthony Cleaver <Anthony.cleaver@mhr.co.uk>
2024-02-28 12:34:39 +0000

    Add command for running eslint on file

--- lua/core/commands.lua
+++ lua/core/commands.lua
@@ -2,3 +2,6 @@ vim.api.nvim_create_user_command("TestDebug", function()
   require('FTerm').run({'npm run test-debug -- --code-coverage --include="'..vim.fn.expand('%:p:~:.')..'"'})
 end, {})
 
+vim.api.nvim_create_user_command("Eslint", function()
+  require('FTerm').run({'npx eslint '..vim.fn.expand('%:p:~:.')})
+end, {})
