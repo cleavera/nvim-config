a19b1f003f35dc761ca5d81688962991b9ad8d25
Anthony Cleaver <Anthony.cleaver@mhr.co.uk>
2024-03-24 18:29:46 +0000

    Use visual line for navigation

--- lua/keymaps/movement.lua
+++ lua/keymaps/movement.lua
@@ -3,6 +3,8 @@ local map = vim.api.nvim_set_keymap
 -- Move up/down on VISUAL line instead of a ACTUAL line
 vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "<down>", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("n", "<up>", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 
 vim.keymap.set("n", "*", "<cmd>let @/='\\<'.expand('<cword>').'\\>'<CR>", { silent = true })
 
