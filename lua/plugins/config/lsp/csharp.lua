b0034ceb169d58c501d2dc08ba322e022dc8d4ef
Anthony Cleaver <Anthony.cleaver@mhr.co.uk>
2024-04-15 10:36:33 +0100

    Add start docker command to c#

--- lua/plugins/config/lsp/csharp.lua
+++ lua/plugins/config/lsp/csharp.lua
@@ -4,6 +4,9 @@ return {
     require("lspconfig").omnisharp.setup({
       on_attach = on_attach,
       capabilities = capabilities,
+      vim.api.nvim_create_user_command("StartDocker", function()
+        require('FTerm').run({'docker compose -f C:\\www\\mhr\\MHR.DotNet.LocalDevelopment\\docker-compose.yml -f C:\\www\\mhr\\MHR.DotNet.LocalDevelopment\\docker-compose.payroll.yml up'})
+      end, {})
     })
   end
 }
