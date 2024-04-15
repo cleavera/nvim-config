fc5376f848a4f50a3bcb9e84aa82a58414d34bd6
Anthony Cleaver <Anthony.cleaver@mhr.co.uk>
2024-04-15 10:36:15 +0100

    Remove test coverage from test debug command

--- lua/core/commands.lua
+++ lua/core/commands.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_user_command("TestDebug", function()
-  require('FTerm').run({'npm run test-debug -- --code-coverage --include="'..vim.fn.expand('%:p:~:.')..'"'})
+  require('FTerm').run({'npm run test-debug -- --include="'..vim.fn.expand('%:p:~:.')..'"'})
 end, {})
 
 vim.api.nvim_create_user_command("Eslint", function()
