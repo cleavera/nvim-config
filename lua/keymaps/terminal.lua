88393bf65477332a2191d9f102b84fc42273f7af
Anthony Cleaver <anthony.cleaver@mhrglobal.com>
2024-07-02 10:24:05 +0100

    Remove extra floating terminals

--- lua/keymaps/terminal.lua
+++ lua/keymaps/terminal.lua
@@ -1,5 +1,4 @@
 local which_key = require("which-key")
-local api = require("plugins.terminal.api")
 which_key.register({
   ["<leader>t"] = { name = "Terminal" }
 })
@@ -13,7 +12,7 @@ end, { desc = "Open" })
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 vim.keymap.set('n', '<Esc>', function ()
-  api.close()
+  fterm.close()
 end, { desc = "Toggle terminal" })
 
 vim.keymap.set('n', '<leader>ts', function()
@@ -21,31 +20,12 @@ vim.keymap.set('n', '<leader>ts', function()
   fterm.scratch({ cmd = cmd })
 end, { desc = "Scratch" })
 
-vim.keymap.set('n', '<leader>tr', function()
-  api.terminals.run:open()
-end, { desc = "Run" })
-
-vim.keymap.set('n', '<leader>tt', function()
-  api.terminals.test:open()
-end, { desc = "Test" })
-
-vim.keymap.set('n', '<leader>g', function()
-  api.terminals.git:open()
-end, { desc = "Git" })
-
 vim.api.nvim_create_autocmd('DirChanged', {
   pattern = {
     '*'
   },
   callback = function()
-    local fterm = require('FTerm')
     fterm.open()
     fterm.exit()
-    api.terminals.test:open()
-    api.terminals.test:close(true)
-    api.terminals.run:open()
-    api.terminals.run:close(true)
-    api.terminals.git:open()
-    api.terminals.git:close(true)
   end,
 })
